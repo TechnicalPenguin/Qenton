58a16b742e7d69d76a82b3bb3efc987e188db0b1
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-19 23:34:11 -0400

    updated libs

--- App.cpp
+++ App.cpp
@@ -1,2 +1,13 @@
 #include "pch.h"
 #include "Engine/Engine.h"
+
+int main(int argc, char** argv)
+{
+	FO3D::Core.Initialize();
+
+	while (FO3D::Core.Run())
+	{
+		FO3D::Core.Update();
+	}
+	return 0;
+}

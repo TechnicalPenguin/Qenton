5b6871157243630e5f734b55eac9ad665b176c4e
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-19 23:25:12 -0400

    Update pch.h

--- pch.h
+++ pch.h
@@ -1,1 +1,34 @@
 #pragma once
+
+// BASIC
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <memory>
+#include <cassert>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+// OPENGL
+#include <GL/glew.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#ifdef FO3D_WINDOW
+
+#endif // FO3D_WINDOW
+
+#ifdef FO3D_LINUX
+
+#endif // FO3D_LINUX
+
+#ifdef FO3D_MAC
+
+#endif // FO3D_MAC

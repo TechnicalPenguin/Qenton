5c5b11311797f5bbcac76064a898d107aabf2475
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-20 09:46:52 -0400

    added window hints

--- Engine.cpp
+++ Engine.cpp
@@ -6,5 +6,11 @@ namespace FO3D
 	Engine::Engine() : run(true), window(NULL), videoWidth(SCREEN_WIDTH), videoHeight(SCREEN_HEIGHT)
 	{
 		glfwInit();
+		//WINDOW HINTS
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	}
 }
